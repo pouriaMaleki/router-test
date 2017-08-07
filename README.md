03df3f540a2781e13e028d5414667e8ee07c6e1e
Pouria Maleki <dev.maleki@gmail.com>
2017-08-07 20:13:22 +0430

    initial router app, containing react router and simple home route

---
+++ README.md
@@ -0,0 +1,4 @@
+Sample React Router V4 With Create React App For Traning Purpose
+
+
+Take look at older commits to see how it developed step by step
---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Home from './Home';
+import { Router, Route } from 'react-router-dom';
+import createBrowserHistory from 'history/createBrowserHistory'
+
+const history = createBrowserHistory()
+
+class App extends Component {
+  render() {
+    return (
+      <Router history={history}>
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Welcome to React</h2>
+          </div>
+          <div className="App-intro">
+            <Route exact path="/home" component={Home} />
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
