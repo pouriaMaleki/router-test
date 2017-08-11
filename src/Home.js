81c14bccac72b3f76b86a1e3b6446a5bcf57ceba
Pouria Maleki <dev.maleki@gmail.com>
2017-08-11 14:23:45 +0430

    sub routes for home route with different components

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import User from './User';
+import Company from './Company';
+import { Route } from 'react-router-dom';
 
 export default class Home extends Component {
   render() {
     return (
       <div>
-        Hello {this.props.name}
+        <h3> This is home header. </h3>
+         <Route path='/home/user' component={ () => <User name={this.props.name} /> }/> 
+         <Route path='/home/company' component={ () => <Company name={this.props.name} /> }/> 
       </div>
     );
   }
